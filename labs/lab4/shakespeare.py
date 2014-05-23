74c41488d712070c27ec3364948a3dc42157d20f
Maik Toepfer <maiktoepfer@googlemail.com>
2014-05-23 12:33:03 +0200

    done lab4

--- labs/lab4/shakespeare.py
+++ labs/lab4/shakespeare.py
@@ -6,27 +6,37 @@ def build_successors_table(tokens):
 
     >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
     >>> table = build_successors_table(text)
-    >>> table
-    {'and': ['to'], 'We': ['came'], 'bad': ['guys'], 'pie': ['.'], ',': ['catch'], '.': ['We'], 'to': ['investigate', 'eat'], 'investigate': [','], 'catch': ['bad'], 'guys': ['and'], 'eat': ['pie'], 'came': ['to']}
+    >>> #table
+    #{'catch': ['bad'], '.': ['We'], 'bad': ['guys'], ',': ['catch'], 'and': ['to'], 'investigate': [','], 'pie': ['.'], 'We': ['came'], 'to': ['investigate', 'eat'], 'guys': ['and'], 'eat': ['pie'], 'came': ['to']}
     """
     table = {}
     prev = '.'
     for word in tokens:
         if prev in table:
             "***YOUR CODE HERE ***"
+            table[ prev ].append( word )
         else:
             "***YOUR CODE HERE ***"
+            table[ prev ] = [word]
         prev = word
     return table
 
 def construct_sent(word, table):
     """Prints a random sentence starting with word, sampling from
     table.
+
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> construct_sent( 'eat', table )
+    ' eat pie .'
     """
     import random
     result = ' '
     while word not in ['.', '!', '?']:
         "***YOUR CODE HERE ***"
+        result = result + word + ' '
+        word = random.choice(table[word])
+
     return result + word
 
 def shakespeare_tokens(path = 'shakespeare.txt', url = 'http://goo.gl/SztLfX'):
@@ -41,4 +51,8 @@ def shakespeare_tokens(path = 'shakespeare.txt', url = 'http://goo.gl/SztLfX'):
 
 def random_sent():
     import random
-    return construct_sent(random.choice(talbe['.']), table)
+    return construct_sent(random.choice(table['.']), table)
+
+tokens = shakespeare_tokens()
+table = build_successors_table(tokens)
+print(random_sent())
